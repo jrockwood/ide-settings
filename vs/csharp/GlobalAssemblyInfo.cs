014f621417b1fb25c5fb69d780672c0aef415a48
Justin Rockwood <justinrockwood@hotmail.com>
2021-02-16 21:56:17 -0800

    Add settings for Visual Studio and C#

---
+++ vs/csharp/GlobalAssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// <copyright file="GlobalAssemblyInfo.cs" company="Justin Rockwood">
+//   Copyright (c) Justin Rockwood. All Rights Reserved. Licensed under the MIT License. See LICENSE.txt in the project
+//   root for license information.
+// </copyright>
+// ---------------------------------------------------------------------------------------------------------------------
+
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+// Company and Product
+[assembly: AssemblyCompany("Justin Rockwood")]
+[assembly: AssemblyProduct("INSERT PRODUCT") // uncomment to compile ]
+[assembly: AssemblyCopyright("Copyright © 2021 Justin Rockwood. All rights reserved.")]
+[assembly: AssemblyTrademark("")]
+
+// Localization
+[assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en-US")]
+
+// Version Information
+[assembly: AssemblyInformationalVersion("1.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+// Other
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("")]
+#endif
+[assembly: ComVisible(false)]
